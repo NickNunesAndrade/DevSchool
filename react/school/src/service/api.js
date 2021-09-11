eeaffb0fdfb1b291bb51be88c4689bf0bc92f963
NickNunesAndrade <niconunes4ndr4de@gmail.com>
2021-09-10 22:49:37 -0300

    Api feita - React

--- react/school/src/service/api.js
+++ react/school/src/service/api.js
@@ -5,7 +5,29 @@ const api = axios.create({
 
 export default class Api {
     async listarNomes()  {
-        let nomes = await api.get('/matricula');
-        return nomes.data;
+        let r = await api.get('/matricula');
+        return r.data;
+    }
+
+    async adicionar(nome, chamada, curso, turma)  {
+        let cria = await api.post('/matricula',{nome, chamada, curso, turma});
+        return cria.data;
+    }
+
+
+    async editar(nome, chamda, curso, turma, id) {
+        let aluno = {
+            nm_aluno: nome,
+            nr_chamada: chamda,
+            nm_curso: curso,
+            nm_turma: turma     
+        }
+        let editar = await api.put(`/matricula/${id}`, aluno);
+        return editar.data;
+    }
+
+    async remover(id) {
+        let remover = await api.delete(`/matricula/${id}`);
+        return remover.data;
     }
 }
